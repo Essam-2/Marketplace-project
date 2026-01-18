49b1c48d222860631d78700f381f1cf19f0ac14f
Essam <esamanwar2@gmail.com>
2026-01-18 22:16:37 +0300

    [+] add product details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './components/products/products';
+import { ProductDetails } from './components/products/product-details/product-details';
 import { OrdersComponent } from './components/orders/orders';
 import { OrderDetails } from './components/orders/order-details/order-details';
 import { UserProfile } from './components/user-profile/user-profile';
@@ -7,6 +8,7 @@ import { UserProfile } from './components/user-profile/user-profile';
 
 export const routes: Routes = [
   { path: 'products', component: ProductsComponent },
+  { path: 'products/:id', component: ProductDetails },
   { path: 'orders', component: OrdersComponent },
   { path: 'orders/:id', component: OrderDetails },
   { path: 'profile', component: UserProfile },
