fd161e322a41f3c413603d252d5a3b17b48c44e7
Essam <esamanwar2@gmail.com>
2026-01-19 10:28:45 +0300

    [+] add guard in the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,17 @@ import { ProductDetails } from './components/products/product-details/product-de
 import { OrdersComponent } from './components/orders/orders';
 import { OrderDetails } from './components/orders/order-details/order-details';
 import { UserProfile } from './components/user-profile/user-profile';
+import { Login } from './components/login/login';
+import { authGuard } from './guards/auth.guard';
 
 
 export const routes: Routes = [
-  { path: 'products', component: ProductsComponent },
-  { path: 'products/:id', component: ProductDetails },
-  { path: 'orders', component: OrdersComponent },
-  { path: 'orders/:id', component: OrderDetails },
-  { path: 'profile', component: UserProfile },
-  { path: '', redirectTo: 'products', pathMatch: 'full' },
-  { path: '**', redirectTo: 'products' },
+  { path: 'login', component: Login },
+  { path: 'products', component: ProductsComponent, canActivate: [authGuard] },
+  { path: 'products/:id', component: ProductDetails, canActivate: [authGuard] },
+  { path: 'orders', component: OrdersComponent, canActivate: [authGuard] },
+  { path: 'orders/:id', component: OrderDetails, canActivate: [authGuard] },
+  { path: 'profile', component: UserProfile, canActivate: [authGuard] },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' },
 ];
