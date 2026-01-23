baee539492d996a09860710ddeb8f3ca71998b99
Essam <esamanwar2@gmail.com>
2026-01-23 23:15:48 +0300

    [-] remove the products details component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { ProductsComponent } from './components/products/products';
-import { ProductDetails } from './components/products/product-details/product-details';
 import { OrdersComponent } from './components/orders/orders';
 import { OrderDetails } from './components/orders/order-details/order-details';
 import { UserProfile } from './components/user-profile/user-profile';
@@ -12,7 +11,6 @@ import { authGuard } from './guards/auth.guard';
 export const routes: Routes = [
 
   { path: 'products', component: ProductsComponent },
-  { path: 'products/:id', component: ProductDetails },
   { path: 'cart', component: CartComponent },
   { path: 'orders', component: OrdersComponent, canActivate: [authGuard] },
   { path: 'orders/:id', component: OrderDetails, canActivate: [authGuard] },
