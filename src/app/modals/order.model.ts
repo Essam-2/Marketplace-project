f8278ae480ded4ecae3f1772d08f0035d2e961a7
Essam <esamanwar2@gmail.com>
2026-01-23 23:16:34 +0300

    [+] update order interfaces

--- src/app/modals/order.model.ts
+++ src/app/modals/order.model.ts
@@ -1,7 +1,25 @@
-export interface Order {
-  id?: string;
+export interface OrderItem {
+  productId: string;
+  productName: string;
+  qty: number;
+  price: number;
+}
+
+export interface OrderCustomer {
+  id: string;
   name: string;
-  price: string;
-  capacity: string;
-  status: string;
+}
+
+export interface OrderTotals {
+  items: number;
+  price: number;
+}
+
+export interface Order {
+  orderId?: string;
+  orderDate?: string;
+  statusText?: string;
+  customer?: OrderCustomer;
+  items: OrderItem[];
+  totals?: OrderTotals;
 }
