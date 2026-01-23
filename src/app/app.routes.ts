413f86583daef8a9dc80a3071e55ff35f92d19f4
Essam <esamanwar2@gmail.com>
2026-01-23 18:46:42 +0300

    [+] add cart route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ProductDetails } from './components/products/product-details/product-de
 import { OrdersComponent } from './components/orders/orders';
 import { OrderDetails } from './components/orders/order-details/order-details';
 import { UserProfile } from './components/user-profile/user-profile';
+import { CartComponent } from './components/cart/cart';
 
 import { authGuard } from './guards/auth.guard';
 
@@ -12,6 +13,7 @@ export const routes: Routes = [
 
   { path: 'products', component: ProductsComponent },
   { path: 'products/:id', component: ProductDetails },
+  { path: 'cart', component: CartComponent },
   { path: 'orders', component: OrdersComponent, canActivate: [authGuard] },
   { path: 'orders/:id', component: OrderDetails, canActivate: [authGuard] },
   { path: 'profile', component: UserProfile, canActivate: [authGuard] },
